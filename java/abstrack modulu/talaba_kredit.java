df0c6f45242f95995c42cbef4c11867a3215e675
DAVRONBEK ABDURAZZOQOV <94719744+21040001@users.noreply.github.com>
2023-01-04 07:48:12 +0300

    Update and rename talaba kredit class fileli to talaba_kredit.java

--- java/abstrack modulu/talaba_kredit.java
+++ java/abstrack modulu/talaba_kredit.java
@@ -1,10 +1,10 @@
-public class talaba extends kreditbase{
+public class talaba_kredit extends kreditbase{
 
 // bu yerda ham extend orqali kredit fayilimizni chaqirib hisobla() kodimizni yozamiz
 
     @Override
     public void hisobla() {
-        System.out.println("talaba");
+        System.out.println("talaba krediti");
         
         // https://www.youtube.com/channel/UCtxypCLZEd8v3AoUu-xZS4w
         
