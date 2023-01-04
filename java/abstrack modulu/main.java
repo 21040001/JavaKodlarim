a769d8fd9a42fbe16c660820c5d8c0deff8a3447
DAVRONBEK ABDURAZZOQOV <94719744+21040001@users.noreply.github.com>
2023-01-04 07:48:55 +0300

    Update main.java

--- java/abstrack modulu/main.java
+++ java/abstrack modulu/main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args){
-        oqtuvchi oqituvchi =new oqtuvchi();
+        oqtuvchi_kredit oqituvchi =new oqtuvchi_kredit();
         oqituvchi.hisobla();
         talaba talaba= new talaba();
         talaba.hisobla();
