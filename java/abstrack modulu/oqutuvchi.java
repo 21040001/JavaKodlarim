e0939eae04c12f9123c8bb2a1d68925409c8366a
DAVRONBEK ABDURAZZOQOV <94719744+21040001@users.noreply.github.com>
2023-01-04 07:46:38 +0300

    Update and rename oqutuvchi kredit class file to oqutuvchi.java

--- java/abstrack modulu/oqutuvchi.java
+++ java/abstrack modulu/oqutuvchi.java
@@ -1,9 +1,9 @@
-public class oqtuvch extends kreditbase{
+public class oqtuvchi extends kreditbase{
 // extend orqali kreditbase abstrack modullu classimzni chaqiramiz
    
    @Override
     public void hisobla() {
-        System.out.println("kredit oqutuvchilar un ");
+        System.out.println("kredit oqutuvchilar uchun ");
         
         // https://www.youtube.com/channel/UCtxypCLZEd8v3AoUu-xZS4w
     }
