4e91304f6fe94aaaf929db5c75d7510d202fb19d
DAVRONBEK ABDURAZZOQOV <94719744+21040001@users.noreply.github.com>
2023-11-23 00:01:56 +0300

    Create BagliClass.java

---
+++ JavaProjelarim/KalkulyatorCalculator/BagliClass.java
@@ -0,0 +1,17 @@
+public class BagliClass   {
+    public double topal(double a, double b){
+        return a+b;
+    }
+    public double eksi(double a, double b){
+        return a-b;
+
+    }
+    public double carpma(double a, double b){
+        return a*b;
+    }
+    public double boluv(double a, double b){
+        return a/b;
+    }
+
+
+    }
